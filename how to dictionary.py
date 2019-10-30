2d27cae98aa3bb622043b9cf3af72ba1640acdb7
zeus <33592162+1nam@users.noreply.github.com>
2019-10-30 07:10:00 -0400

    Create how to dictionary.py

---
+++ how to dictionary.py
@@ -0,0 +1,10 @@
+# dictionary to store data
+nick_names ={
+    "jason"    : "jay",
+    "kenneth"  : "ken",
+    "Jacob"    : "Coby",
+    "William"  : "Bill",
+    "Alexander": "Alex",
+    
+}
+print(nick_names.get("Alexander", "key not found")) #the key with get function
